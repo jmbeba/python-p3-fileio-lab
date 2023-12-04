7a1224cb6ee405c6e8cfab0a2c5e3575866b6ab3
jmbeba <john.mbeba@student.moringaschool.com>
2023-12-04 16:14:23 +0300

    lab completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w') as txt_file:
+        txt_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a') as txt_file:
+        txt_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt") as txt_file:
+        return txt_file.read()
